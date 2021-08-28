1afa4b0cb3ea05878e8415f871b42bfbe1351ab3
João Amaral <jamaral7@gmail.com>
2021-08-28 17:54:55 +0900

    installation texts, several bug fixes, progress bar

---
+++ js/texts.js
@@ -0,0 +1,82 @@
+var all_you_can_eat = `<div class="text">
+<h1>ALL YOU CAN EAT</h1>
+<p>PAINTING INSTALLATION BY CARLOS BARAHONA POSSOLO</p>
+<p><br></p>
+<p>Of all our senses, taste is probably the one through which we establish the most intimate relationship with the world. We realize what things taste of when, in a primordial communion, we introduce them into our mouths. We make what we taste our own, either through simple exploration, or by swallowing and digesting it so that it becomes an integral part of ourselves. Our need to feed and the predatory way in which we are forced to do so, have dominated our evolution for hundreds of millions of years. We daily partake in thisblessed or cursed alchemical ceremony of ingesting something which is alien to us to make it our very own.</p>
+<p><br></p>
+<p>The mouth and particularly the tongue is the organ in which the process begins and in which it is most stimulating. A whole universe of expectations is transformed when we finally taste what we had only observed through sight and touch. It may work in concourse with touch; it may, indeed, be a visceral form of touch which generates complete interpenetration and develops a particularly strong network of connections which are bound to our own physiology. The mouth, the second embryological opening to develop in humans, is a magical place. Through it we attempt to control foods by chewing and are made most vulnerable to their effects on our body or on our mind. It is an area of tension between the self and the other.</p>
+<p><br></p>
+<p>The mouth is intrinsically linked to sex. A place of strife and interaction where pleasure is given and received. The word "satisfaction" resumes the mouth's fundamental role: satisfaction of one's appetite, be it hunger or libido. It is striking that such a rich and complex organ is so exposed and that we constantly open it to the world to kiss, to speak, to eat, or to sigh. It is an organ of total promiscuity that we use for everything, simultaneously allowing us to communicate and to devour, to kiss and to bite. It combines both the eternal principles: Pleasure-Eros, that of the soft, wet and fluid tongue and Death-Thanatos of the threatening, mineral teeth. It is no surprise that we are ashamed of showing our tongue: we know it is a sexual organ.</p>
+<p><br></p>
+<p>My installation project focuses on the seven tastes which are traditionally detected by the tongue: sour, bitter, sweet, salty, pungent (spicy hot), metallic and umami. Each of these principles is enacted by a group of intimately interwined figures that explore one another in various ways. I want to stress the connection of the mouth and the tongue with sex and its role as an organ which is primarily centred on pleasure. Hence the figuration of the genital anatomy which actively appears in the scenes. My aim is to reflect on the total continuity between all things linked to libido, the universal principle which unfolds and materialises in ever-changing ways to each of us. Based on the theory that our lives are immersed in a vast ocean of libido, from which emancipation is extremely difficult and would entail the risk of annihilation, I have tried to explore a few erotic variations of the extraordinary experiences our mouths can bring us.</p>
+<p><br></p>
+<p>This group of paintings is protected by the sacred symbolism of the number 7. Although profane and pagan in appearance, it aims to be a hymn to the mystery of life which constrains us to existing in flesh while perceiving the spirit, in a stark contradiction which is the core issue of our condition. In a way, these paintings are examples of vanitas because they evidence how we are kept captive by things material that act as aprioristic limitations in which the miracle of transcendence is revealed.</p>
+<p><br></p>
+<p>The title All You can Eat indicates unrestraint and excess. These are the uncertain boundaries beyond which one incurs in Hubris ( the infringement of limits which will unleash Nemesis or punishment), in a permanent contrast with the principle of guilt which in varying degrees torments us all through the lasting bonds to the norms of the culture we grew up in.</p>
+<p><br></p>
+<p>British art critic Cefyn Embling-Evans, upon viewing these seven paintings, coined a new conceptual category to classify them: Renaissance Porn, which combines various formal contents in a way I particularly like.</p>
+<p><br></p>
+<!-- <p>Download catalogue (Portuguese)</p> -->
+</div>`;
+
+var behind_the_green_door = `<div class="text">
+<h1>BEHIND THE GREEN DOOR</h1>
+<br>
+<p>PAINTING INSTALLATION BY BARAHONA POSSOLO</p>
+<br>
+<p>My desire to go further in dismantling prejudice was at the inception of this show/painting installation. Sexuality is an area that is still riddled with restrictions and dogmas that were artificially laid down by the dominant judeo- christian ideology. Many of us continually strive to boost other viewpoints into this vast territory, be they long-lived or cutting-edge. The demonization of sexuality as a fallacious threat to the cohesion of Civilization, which has caused so much suffering to humankind, is even more emphatic in the case of the less frequent fringe practices often referred to as “fetishes”. Many call them derangements, in an acrid judgement hidden under the cloak of chastity and prejudice.</p>
+<p>Demonization has always been used to suppress attitudes that have the power to imperil the ‘status quo’ or that are simply unfamiliar . The truth is that social dominance and power structures, which are often based on imaginary myths that are nothing but instruments of control, are liable to change. Those who hold power (and who are obviously clever enough to do so), will always try to buffer change so that it ensures the maintenance of power in the same hands.</p>
+<p>This Art project wishes to reclaim a series of sexual practices from the territory of transgression. By classifying them with the alchemical symbols of the five natural elements, I deliberately use a code that normally applies to concepts which are considered “sacred” in many cultures. This may be seen as blasphemy. Yet it seeks to underline that all levels of human existence bear the same true essence. Such is the case of the most complex aspects of sexual behaviour. In trying to integrate practices which, for most people, are usually banned (at least publically), I pursue the completion of the huge mosaic that is ‘Humankind’ with its last tesserae.</p>
+<p>The pursuit to integrate reality is also evident here in the depiction of very diverse physical types, in an effort to counter the brutal aesthetical dictatorship, which is supported by the media and dominant commercial interest.</p>
+<p>The gold-relief (gold being an essential symbol) alchemical “Seal” of the elements at the centre of each picture acts as an amulet that helps cast out any haunting dark shadows.  </p>
+<p>In this series, fetishes are depicted through homosexual scenes. Some will argue that it is narrow-minded and misleading. I say it is just a variation- I’d rather delve into what I know- and gender differences tend to fade out in the case of fetishes, anyway. The fact that these are gay scenes may just add “spice” to the whole affair, in the case of those who are more conservative and old-fashioned, regardless of age.</p>
+<p>The venue for this installation is a subterranean ex-public lavatory (a “cottage”). This is a fundamental fact, in this case. As is common knowledge, many public toilets and urinals were the location of male transgressional sexual encounters and were often raided and closed down due to acts of ‘gross indecency’. Once again the demonization of such attitudes sought solely to perpetuate traditional gender ideology, as we all know. </p>
+<p>As we literally go ‘underground’ to face such mysteries, unholy to some, sacred to others, we walk a path of social psychoanalysis. A path which is part of a journey that is yet to be outlined:  that of Male Liberation. After Women’s Liberation which, though very incomplete, is generally seen as imperative and crucial, and Gay/LGBT Liberation which has gained significant acceptance, Male Liberation will be necessary to complete the previous two. By this I mean that if women have burned and shredded the absurd, violent script that enslaved them and gay people have refused the wretched place in society that was ascribed to them, men (mainly heterosexual ‘macho’ men) need to free themselves from the ridiculous yet brutal code that is imposed upon them as ‘masculinity’.</p>
+<p>I talk of the Present. Now. When, in spite of all possibilities, being ‘a Man’ is still the screenplay of a violent film in which everyone, starting with every young boy, is forcefully ravished. This liberation will allow every possible gender category to flourish: as many as there are people. Each one of us shares the same binding humanity but is a unique and inimitable being.</p>
+<p>‘Civilization’ still needs to encompass individual expression in a fine balance between the self and others.</p>
+<p>Carlos Barahona Possollo</p>
+<p>Lisbon, 24th August 2020</p>
+<p><br></p>
+
+<hr>
+
+<p><b>Behind The Green Door- Painting Installation by Barahona Possollo</b></p>
+<h1>SEX AND POLITICS</h1>
+<p>It would be virtually impossible not to approach this painting installation through its social and political significance, both due to its contents and to its very distinctive setting.</p>
+<p>Why evoke the 5 natural elements, today, in painted depictions of 5 gay sexual fetishes? Why show them in an underground ex-public urinal that was notorious, in its day, as a place of secret sexual encounters between men, often raided by the police, in the most central area of the capital city of what was the first Christian colonial empire in the world?</p>
+<p>The answer to these questions should probably begin by stressing the clear social and political implications of such choices.</p>
+<p>To consider the fairly recent changes in attitude towards sexuality, particularly gay sexuality, within western societies, the reason why this show is possible here and now, falls evidently short.</p>
+<p>The international LGBT liberation movement, though rebellious and confrontational at the outset, in the Stonewall days, gradually opted for a conciliatory public image, demanding equal rights for hetero and homo (and later non-hetero) sexual orientations: - equal rights to love, to build families and to marry -, with forbearance and equanimity…</p>
+<p>The overcoming of the patriarchal model and the <b>subversion</b> of the ‘status quo’ that accession to such rights by couples of women, of men (or of people who consider themselves neither) necessarily meant, was deliberately sidelined.</p>
+<p>Tolerance was, often explicitly, called for. Tolerance, however, by implying condescendence on behalf of the stronger party, - to whom one should be thankful-, establishes a hierarchy.</p>
+<p>(Tolerance for Jews is what Heidegger asked of the Nazi Germans in order to avoid turning them into the martyrs of history...).</p>
+<p>By seeming to willingly concede rights to homosexuals, the traditional patriarchy felt safeguarded while claiming a clear conscience.</p>
+<p>Now, this installation ostensibly discards tolerance.</p>
+<p>It takes on the taboo of fetish without flinching.</p>
+<p>By portraying it in a gay context, it both discloses and embraces the abominable practices that traditionalist bigots have always attributed to ‘homosexual perversion’.</p>
+<p>By placing it in a subterranean disused public lavatory, it confronts the dominant social model with the centuries of exclusion, sexual repression, abuse, cruelty and injustice, it subjected (and subjects) homosexuals to.</p>
+<p>By matching each fetish with an element, it gives it the natural, uncomplicated quality of spontaneity. It effectively dismantles the notion that paraphilia is a mental disorder, which, in any case, is far from being unanimous among psychiatrists, psychologists and sexologists.</p>
+<p>By bringing them to the ‘Marquês de Pombal’, the very centre of Lisbon, it renders themvisible, releasing them from the concealment that Christian ideology has long forced upon them.</p>
+<p>This painting installation asserts the political character of sex.</p>
+<p>In the Freudian-Marxist perspective of Wilhelm Reich, it stresses that control, even suppression, ofsexual behaviours is crucial in shaping each individual psyche in accordance with the repressive and authoritarian principles of class society, so as to ensure the hereditary lines of property. This happens mainly through socialization in children and adolescents, but equally through social coercion in adults.</p>
+<p>If chastity was initially imposed on the ruling classes, especially on patrician women, sexual repression became the rule in every social class, as the ideology of the dominant classes is both hegemonic and aspirational.</p>
+<p>Likewise, rejection by parts of the gay community (at least in public) of any connection with what are considered more ‘deviant’ practices, reveals its internalization of sexual repression and its adherence to the model of ‘granted’ tolerance.</p>
+<p>Echoing Herbert Marcuse’s concept of ‘civilization being based on the constant subjection of human instinct’, it is the struggle to overcome<b> all</b> sexual repression that is summoned here.</p>
+<p>It alone may result in a radically different future.</p>
+<p>Leonel Pedroso Gonçalves</p>
+<p>Sociologist</p>
+<p>Lisbon, 25th August 2020</p>
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
